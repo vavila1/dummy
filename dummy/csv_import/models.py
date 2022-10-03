7f20edfab25410036af9113738b978f541bef109
Victor Avila <victoravila@MacBook-Pro-de-Victor.local>
2022-10-03 13:06:01 -0500

    Agregando BD

--- dummy/csv_import/models.py
+++ dummy/csv_import/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    matricula = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.id) + '-' + self.matricula
